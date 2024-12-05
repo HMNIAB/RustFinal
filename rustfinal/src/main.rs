46ac0287db5e19cfd833583687b6d37739e35fcd
HMNIAB <beauteravery@hotmail.com>
2024-12-05 14:46:25 -0700

    request handling
    basic request handling, server will increment the counter and logs the result

--- rustfinal/src/main.rs
+++ rustfinal/src/main.rs
@@ -2,6 +2,7 @@ use log::{info};
 use env_logger;
 use std::sync::{Arc, Mutex};
 use tokio;
+use std::time::Duration;
 
 #[derive(Clone)]
 struct Server {
@@ -14,6 +15,12 @@ impl Server {
             counter: Arc::new(Mutex::new(0)),
         }
     }
+    async fn handle_request(&self) { //sim request handling with a sleep of 100ms
+        tokio::time::sleep(Duration::from_millis(100)).await;
+        let mut counter = self.counter.lock().unwrap(); //lock the counter and increment
+        *counter += 1;
+        info!("Handled request, counter: {}", *counter);
+    }
 }
 
 #[tokio::main]
@@ -22,4 +29,5 @@ async fn main() {
     info!("Program started.");
     let server = Server::new();
     info!("Sever initialized.");
+    server.handle_request().await; //handle simmed request
 }
